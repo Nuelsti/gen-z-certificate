194613f7be0a4813e5c05b491e89e12c2c891bd7
heart <150511179+Nuelsti@users.noreply.github.com>
2025-07-14 11:21:28 +0100

    added-env

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -7,14 +7,16 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBmI9LUa_iBMPtF_oTfv02HUGD506mHtS8",
-  authDomain: "gen-z-certificate-generator.firebaseapp.com",
-  projectId: "gen-z-certificate-generator",
-  storageBucket: "gen-z-certificate-generator.firebasestorage.app",
-  messagingSenderId: "847457863862",
-  appId: "1:847457863862:web:a5e5561558568e5e733e17",
-  measurementId: "G-E7MEM63460"
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
